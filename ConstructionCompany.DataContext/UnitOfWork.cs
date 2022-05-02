9519dc52c9c7386c637cd115979ea7e007000d0f
PetarPaskas <petar.paskas@hotmail.com>
2022-05-02 14:36:19 +0200

    setting up unit of work properties

--- ConstructionCompany.DataContext/UnitOfWork.cs
+++ ConstructionCompany.DataContext/UnitOfWork.cs
@@ -8,5 +8,19 @@ namespace ConstructionCompany.DataContext
 {
     public class UnitOfWork : IUnitOfWork
     {
+        public ICityRepository Cities { get; set; }
+        public IClientRepository Clients { get; set; }
+        public IConstructionSiteRepository ConstructionSites { get; set; }
+        public ICurrencyRepository Currencies { get; set; }
+        public IEarlyPaymentRepository EarlyPayments { get; set; }
+        public IMunicipalityRepository Municipalities { get; set; }
+        public INoteRepository Notes { get; set; }
+        public IPaymentTypeRepository PaymentTypes { get; set; }
+        public IProfessionRepository Professions { get; set; }
+        public IRoleRepository Roles { get; set; }
+        public IUserRepository Users { get; set; }
+        public IUserRoleRepository UserRoles { get; set; }
+        public IWageRepository Wages { get; set; }
+        public IWorkTypeRepository WorkTypes { get; set; }
     }
 }
