6282aba19c8845cfcd7410e03f52feb1bef22a40
PetarPaskas <petar.paskas@hotmail.com>
2022-06-02 17:18:32 +0200

    Setting up http pipeline and services

--- ConstructionCompany.WebAPI/Startup.cs
+++ ConstructionCompany.WebAPI/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using ConstructionCompany.DataContext;
 using ConstructionCompany.DataContext.Globals;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json;
 
 namespace ConstructionCompany.WebAPI
 {
@@ -13,15 +17,53 @@ namespace ConstructionCompany.WebAPI
 
             services.AddCors();
 
-            services.AddControllers();
+            services
+                .AddControllers()
+                .AddJsonOptions(options =>
+                {
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                });
 
             services.AddConstructionCompanyContext();
 
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(name: "v1", info: new OpenApiInfo
+                { Title = "Northwind Service API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHsts();
 
+            app.UseRouting();
+
+            app.UseCors(options =>
+            {
+                options.WithMethods("GET");
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json",
+                "ConstructionCompany Service API Version 1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+            });
         }
     }
 }
